646dd5d1e17563857fdae5627a97de203e291243
Andrea Voulgaris <abv@umich.edu>
2022-11-21 18:23:43 -0500

    commit tree 8000e11a13cf6c09a188705952f5a3fdaf2dad1a

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride < 400) {
+    return "This one is on me!";
+  }
+  else if (ride > 400 && ride < 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (ride >= 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (ride > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(NYC){
+  return "NYC" === NYC ? ("Ok, sounds good.") : ("No go."); 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(response){
+  switch (response) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";    
+  }
 }
